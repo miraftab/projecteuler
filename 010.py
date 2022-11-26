f2ddc970dd17ab7937ea95fb0900094c62424f07
Javad Miraftabzadeh <javad.miraftabzadeh@gmail.com>
2022-11-26 17:27:55 +0300

    rename and update problem 10

--- 010.py
+++ 010.py
@@ -1,12 +1,12 @@
-# Problem 10: Summation of primes
-marked = [0] * 2000000
+# https://projecteuler.net/problem=10: Problem 10: Summation of primes
+marked = [0] * 2_000_000
 value = 3
 s = 2
-while value < 2000000:
+while value < 2_000_000:
     if marked[value] == 0:
         s += value
         i = value
-        while i < 2000000:
+        while i < 2_000_000:
             marked[i] = 1
             i += value
     value += 2
